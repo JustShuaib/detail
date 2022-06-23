883ed5f5f0e1b41203271296e97270aeb31be7a1
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-06-23 12:27:16 +0100

    worked on the javaScript

---
+++ script.js
@@ -0,0 +1,6 @@
+const myName = "Adeoti Shuaib";
+const height = "5.4ft";
+const country = "Nigeria";
+console.log("My name is " + myName);
+console.log("My height is " + height);
+console.log("My country is " + country);
